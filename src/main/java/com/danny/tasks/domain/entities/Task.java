6bdbfe59d66a7429dffcf18b160c427b3d896086
barakadanny <barakadan421@gmail.com>
2025-07-24 16:54:13 +0200

    task class

---
+++ src/main/java/com/danny/tasks/domain/entities/Task.java
@@ -0,0 +1,165 @@
+package com.danny.tasks.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="due_date")
+    private LocalDateTime dueDate;
+
+    @Column(name="status", nullable = false)
+    private TaskStatus status;
+
+    @Column(name="priority", nullable = false)
+    private TaskPriority priority;
+
+    @ManyToOne(fetch = FetchType.LAZY) //Task list won't be loaded fromDB until it is needed
+    @JoinColumn(name="task_list_id")
+    private TaskList tasklist;
+
+    @Column(name="created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name="updated", nullable = false)
+    private LocalDate updated;
+
+    public Task() {
+    }
+
+    public Task(
+            UUID id,
+            String title,
+            String description,
+            LocalDateTime dueDate,
+            TaskStatus status,
+            TaskPriority priority,
+            TaskList tasklist,
+            LocalDateTime created,
+            LocalDate updated) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+        this.tasklist = tasklist;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public TaskList getTasklist() {
+        return tasklist;
+    }
+
+    public void setTasklist(TaskList tasklist) {
+        this.tasklist = tasklist;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDate getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDate updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title) && Objects.equals(description, task.description) && Objects.equals(dueDate, task.dueDate) && status == task.status && priority == task.priority && Objects.equals(tasklist, task.tasklist) && Objects.equals(created, task.created) && Objects.equals(updated, task.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, dueDate, status, priority, tasklist, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", status=" + status +
+                ", priority=" + priority +
+                ", tasklist=" + tasklist +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
