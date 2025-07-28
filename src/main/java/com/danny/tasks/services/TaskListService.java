dbf7943ba84f71c3e61f91231267ed2c83ad2d2e
barakadanny <barakadan421@gmail.com>
2025-07-28 17:20:52 +0200

    getTaskList method interface

--- src/main/java/com/danny/tasks/services/TaskListService.java
+++ src/main/java/com/danny/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.danny.tasks.services;
 import com.danny.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskList();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
